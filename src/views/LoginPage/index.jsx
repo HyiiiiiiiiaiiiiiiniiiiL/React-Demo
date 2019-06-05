aab84cd4d472d17d2ad4fbe7901536ef6fb7fdb4
HiiYiiLii <892630853@qq.com>
2019-06-05 10:05:26 +0800

    eslintrc配置修改

--- src/views/LoginPage/index.jsx
+++ src/views/LoginPage/index.jsx
@@ -1,13 +1,26 @@
+//cSpell:word srctop
 import React, { Component } from 'react'
 import Button from "../../common/components/Button"
 import Field from "../../common/components/Filed"
+import PropTypes from "prop-types"
 import "./index.styl"
 const src = require("../../resources/login_left_image.png")
 const srctop = require("../../resources/login_rocket.png")
 
 export default class LoginPage extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isPasswordLogin: false,
+            isCodeLogin: false,
+            isSign: false,
+            showChangeAndResendButtons: false,
+            haveSendCode: false,
+            codeRight: false
+        }
+    }
 
-    renderLogin() {
+    renderLogin = () => {
         return <div className="login-render">
             <div className="login-title">验证码登录</div>
             <Field type="input" className="phone-email-input" placeholder="手机号/邮箱地址" />
@@ -25,11 +38,44 @@ export default class LoginPage extends Component {
             <Button className="sign-button">没有账号?去注册></Button>
         </div>
     }
-    renderSign() {
-        return <div className="sign-render"></div>
+    renderSecondTitle = () => {
+
     }
-    renderReset() {
-        return <div className="reset-render"></div>
+    renderResetOrSign() {
+        const { showChangeAndResendButtons, isSign, haveSendCode, codeRight } = this.state
+        return <div className="sign-reset">
+            <div className="second-title">
+                <span>请输入您的手机号或邮箱地址作为您的账号</span>
+                <span>我们发送了一条短信/邮件到您的手机号/邮箱地址</span>
+                <span>请设置您的密码</span>
+            </div>
+            <div className="third-title">
+                <span>请输入短信/邮件中的<strong>6位验证码</strong></span>
+            </div>
+            <Field type="input" className="phone-email-input" placeholder="手机号/邮箱地址" />
+            <Button>发送验证码</Button>
+            {
+                showChangeAndResendButtons ?
+                    <React.Fragment><Button>更改我的手机号/邮箱</Button> <Button>重新发送验证码</Button></React.Fragment>
+                    : <Button>已有账号,去登录></Button>
+            }
+        </div>
+    }
+    renderTitle() {
+        const { isPasswordLogin, isCodeLogin, isSign } = this.state
+        return <div className="login-title">
+            <div className="login-title">
+                {
+                    isPasswordLogin ? "登录" : isCodeLogin ? "验证码登录" : isSign ? "注册" : "重置密码"
+                }
+            </div>
+        </div>
+    }
+    renderContent() {
+
+    }
+    renderButton() {
+
     }
     render() {
         return <div className="login-page">
@@ -48,4 +94,8 @@ export default class LoginPage extends Component {
             </div>
         </div>
     }
+}
+
+LoginPage.propTypes = {
+
 }
